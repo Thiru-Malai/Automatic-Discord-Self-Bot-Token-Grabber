a41da04572b868c5f73ce9e267e425627459060b
Thirumalai K.G <malaithiru370@gmail.com>
2023-02-05 23:37:51 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Helloo Script")
+TOKEN = ""
+window.webpackChunkdiscord_app.push([
+    [Math.random()],
+    {},
+    req => {
+      for (const m of Object.keys(req.c)
+        .map(x => req.c[x].exports)
+        .filter(x => x)) {
+        if (m.default && m.default.getToken !== undefined) {
+          TOKEN = m.default.getToken()
+          // return copy(m.default.getToken());
+        }
+        if (m.getToken !== undefined) {
+          TOKEN = m.getToken()
+          // return copy(m.getToken());
+        }
+      }
+    },
+  ]);
+  console.log(TOKEN)
+  // copy(TOKEN);
+
+  navigator.clipboard.writeText(TOKEN).then(() => {
+    console.log('Content copied to clipboard');
+    /* Resolved - text copied to clipboard successfully */
+  },() => {
+    console.error('Failed to copy');
+    /* Rejected - text failed to copy to the clipboard */
+  });
+
+  console.log('%cWorked!', 'font-size: 50px');
+  console.log(`%cYou now have your token in the clipboard!`, 'font-size: 16px');
